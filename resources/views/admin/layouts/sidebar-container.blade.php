f4c83c3fa7d1ddb1162efbd968b434f994a85c22
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2023-12-24 01:51:36 +0700

    sidebar-container Admin change main-sidebar-custom

--- resources/views/admin/layouts/sidebar-container.blade.php
+++ resources/views/admin/layouts/sidebar-container.blade.php
@@ -1,4 +1,6 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+{{-- <aside class="main-sidebar sidebar-dark-primary elevation-4"> --}}
+<aside class="main-sidebar main-sidebar-custom sidebar-dark-primary elevation-4"> {{-- main-sidebar-custom --}}
+
     <!-- Brand Logo -->
     <a href="/dashboard/overview" class="brand-link">
         <img src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
@@ -709,6 +711,13 @@
         <!-- /.sidebar-menu -->
     </div>
     <!-- /.sidebar -->
+
+    {{-- main-sidebar-custom --}}
+    <div class="sidebar-custom">
+        <a href="#" class="btn btn-link"><i class="fas fa-cogs"></i></a>
+        <a href="#" class="btn btn-secondary hide-on-collapse pos-right">Help</a>
+    </div>
+    <!-- /.sidebar-custom -->
 </aside>
 
 <script>
